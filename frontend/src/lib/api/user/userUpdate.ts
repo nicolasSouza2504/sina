325090994e1aa3eeb9d347945de19cbe452576f7
Gustavolskw <gustavolschmidt13@gmail.com>
2025-09-22 21:56:57 -0300

    feat: tela de perfil e update funcional

---
+++ frontend/src/lib/api/user/userUpdate.ts
@@ -0,0 +1,44 @@
+"use server";
+import getApiBaseUrl from "@/lib/api/api";
+import { UserUpdate } from "@/lib/interfaces/userInterfaces";
+import { getTokenFromSession } from "@/lib/auth/jwtAuth";
+
+export default async function UpdateUserService(
+    userData: UserUpdate,
+    id: number,
+    image: File | null
+) {
+    const base = getApiBaseUrl();
+    const token = await getTokenFromSession();
+
+    const formData = new FormData();
+
+    formData.append("user", JSON.stringify(userData));
+
+    if (image) formData.append("image", image);
+
+    const response = await fetch(`${base}/user/update/${id}`, {
+        method: "PUT",
+        headers: {
+            // NÃO defina Content-Type manualmente
+            Authorization: token, // ou `Bearer ${token}`
+            Accept: "application/json",
+        },
+        body: formData,
+    });
+
+    if (!response.ok) {
+        let msg = "Erro ao Atualizar usuário";
+        try {
+            const err = await response.json();
+            msg = err?.message ?? msg;
+        } catch {}
+        if (typeof window !== "undefined") {
+            const { toast } = await import("sonner");
+            toast(msg);
+        }
+        throw new Error(msg);
+    }
+
+    return response.json();
+}
