997912da2de35851ace354d477c3e8ed65f2c7b6
nicolas <nicolas.souza@moinformatica.com>
2025-09-22 21:42:38 -0300

    feat: Course CRUD

--- backend/java/ava_senai/src/main/java/senai/com/ava_senai/controller/CourseController.java
+++ backend/java/ava_senai/src/main/java/senai/com/ava_senai/controller/CourseController.java
@@ -7,7 +7,6 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import senai.com.ava_senai.domain.course.CourseRegisterDTO;
 import senai.com.ava_senai.domain.course.CourseResponseDTO;
-import senai.com.ava_senai.exception.UserAlreadyExistsException;
 import senai.com.ava_senai.exception.UserNotFoundException;
 import senai.com.ava_senai.repository.CourseRepository;
 import senai.com.ava_senai.response.ApiResponse;
@@ -60,11 +59,11 @@ public class CourseController {
 
         try {
 
-            CourseResponseDTO courseResponseDTO = courseService.updateUser(courseRegisterDTO, courseId);
+            CourseResponseDTO courseResponseDTO = courseService.updateCourse(courseRegisterDTO, courseId);
 
             return ResponseEntity.ok().body(new ApiResponse("Curso editado com sucesso", courseResponseDTO));
 
-        } catch (UserAlreadyExistsException e) {
+        } catch (Exception e) {
             return ResponseEntity.status(409).body(new ApiResponse(e.getMessage(), null));
         }
 
