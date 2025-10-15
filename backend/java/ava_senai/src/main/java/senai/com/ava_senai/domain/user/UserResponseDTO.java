d666e3f922e3c60a5d43f87ea4b444d62b31b434
Gustavolskw <gustavolschmidt13@gmail.com>
2025-10-15 19:10:52 -0300

    feat: rebase

--- backend/java/ava_senai/src/main/java/senai/com/ava_senai/domain/user/UserResponseDTO.java
+++ backend/java/ava_senai/src/main/java/senai/com/ava_senai/domain/user/UserResponseDTO.java
@@ -2,11 +2,10 @@ package senai.com.ava_senai.domain.user;
 
 import senai.com.ava_senai.domain.user.role.Role;
 
-public record UserResponseDTO(Long id, String email, String nome, UserStatus status, List<String> roles) {
 
-public record UserResponseDTO(Long id, String email, String nome, Role role, String institutionName, String cpf ) {
+public record UserResponseDTO(Long id, String email, String nome, UserStatus status,  Role role, String institutionName, String cpf ) {
     public UserResponseDTO(User user ) {
-        this(user.getId(), user.getEmail(), user.getName(), user.getRole(), user.getInstitution().getInstitutionName(),  user.getCpf(), user.getNameImage());
+        this(user.getId(), user.getEmail(), user.getName(), user.getUserStatus(), List.of(user.getRole().getName()));
         this(user.getId(), user.getEmail(), user.getName(), user.getRole(), user.getInstitution().getInstitutionName(),  user.getCpf(), user.getNameImage());
     }
 }
