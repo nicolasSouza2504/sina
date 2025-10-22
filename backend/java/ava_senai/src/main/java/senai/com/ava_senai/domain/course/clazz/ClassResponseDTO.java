702ffb584ac88c2542d9e9d363cdcd414de5eb60
nicolas <nicolas.souza@moinformatica.com>
2025-10-22 20:44:39 -0300

    feat: Ajustes Integration course

--- backend/java/ava_senai/src/main/java/senai/com/ava_senai/domain/course/clazz/ClassResponseDTO.java
+++ backend/java/ava_senai/src/main/java/senai/com/ava_senai/domain/course/clazz/ClassResponseDTO.java
@@ -4,8 +4,18 @@ import senai.com.ava_senai.domain.course.Course;
 
 import java.time.LocalDate;
 
-public record ClassResponseDTO(Long Id, String nome, LocalDate startDate, LocalDate finalDate, String imgClass, Integer semester, String code, Course course) {
+public record ClassResponseDTO(Long Id, String nome, LocalDate startDate, LocalDate finalDate, String imgClass, Integer semester, String code, CourseSimpleResponseDTO course) {
+
     public ClassResponseDTO(Class clazz) {
-        this(clazz.getId(), clazz.getName(), clazz.getStartDate(), clazz.getEndDate(), clazz.getImgClass(), clazz.getSemester(), clazz.getCode(), clazz.getCourse());
+        this(clazz.getId(), clazz.getName(), clazz.getStartDate(), clazz.getEndDate(), clazz.getImgClass(), clazz.getSemester(), clazz.getCode(), new CourseSimpleResponseDTO(clazz.getCourse()));
     }
+
+    public record CourseSimpleResponseDTO(Long id, String name) {
+
+        public CourseSimpleResponseDTO(Course course) {
+            this(course.getId(), course.getName());
+        }
+
+    }
+
 }
