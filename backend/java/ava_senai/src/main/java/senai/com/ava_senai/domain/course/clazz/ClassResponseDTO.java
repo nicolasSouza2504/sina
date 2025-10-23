5aa08d2ba9a66eb63e786191a4c54092cc0b84c7
nicolas <nicolas.souza@moinformatica.com>
2025-10-22 21:40:48 -0300

    feat: Ajustes Integration course

--- backend/java/ava_senai/src/main/java/senai/com/ava_senai/domain/course/clazz/ClassResponseDTO.java
+++ backend/java/ava_senai/src/main/java/senai/com/ava_senai/domain/course/clazz/ClassResponseDTO.java
@@ -1,19 +1,17 @@
 package senai.com.ava_senai.domain.course.clazz;
 
-import senai.com.ava_senai.domain.course.Course;
-
 import java.time.LocalDate;
 
 public record ClassResponseDTO(Long Id, String nome, LocalDate startDate, LocalDate finalDate, String imgClass, Integer semester, String code, CourseSimpleResponseDTO course) {
 
     public ClassResponseDTO(Class clazz) {
-        this(clazz.getId(), clazz.getName(), clazz.getStartDate(), clazz.getEndDate(), clazz.getImgClass(), clazz.getSemester(), clazz.getCode(), new CourseSimpleResponseDTO(clazz.getCourse()));
+        this(clazz.getId(), clazz.getName(), clazz.getStartDate(), clazz.getEndDate(), clazz.getImgClass(), clazz.getSemester(), clazz.getCode(), new CourseSimpleResponseDTO(clazz));
     }
 
     public record CourseSimpleResponseDTO(Long id, String name) {
 
-        public CourseSimpleResponseDTO(Course course) {
-            this(course.getId(), course.getName());
+        public CourseSimpleResponseDTO(Class clazz) {
+            this(clazz.getCourseId(), clazz.getCourse() != null ? clazz.getCourse().getName() : null);
         }
 
     }
