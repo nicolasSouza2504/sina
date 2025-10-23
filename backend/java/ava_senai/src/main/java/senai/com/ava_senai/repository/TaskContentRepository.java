77bf69b48b2f140907f5cd46661f521053d80ddb
nicolas <nicolas.souza@moinformatica.com>
2025-10-23 20:09:33 -0300

    feat: Ajustes Integration course

--- backend/java/ava_senai/src/main/java/senai/com/ava_senai/repository/TaskContentRepository.java
+++ backend/java/ava_senai/src/main/java/senai/com/ava_senai/repository/TaskContentRepository.java
@@ -8,5 +8,4 @@ import java.util.Optional;
 
 @Repository
 public interface TaskContentRepository extends JpaRepository<TaskContent, Long> {
-    Optional<TaskContent> findByIdentifier(String identifier);
 }
