1eec80ad190e9916e3451a6669bc683bd31d055f
nicolas <nicolas.souza@moinformatica.com>
2025-11-04 19:59:56 -0300

    feat: Criação content summary DTO

--- backend/java/ava_senai/src/main/java/senai/com/ava_senai/domain/course/CourseResponseDTO.java
+++ backend/java/ava_senai/src/main/java/senai/com/ava_senai/domain/course/CourseResponseDTO.java
@@ -11,7 +11,11 @@ public record CourseResponseDTO(String name, Integer quantitySemester, Long id, 
     public CourseResponseDTO(Course course) {
         this(course.getName(), course.getQuantitySemester(), course.getId(),
                 course.getClasses() != null ? course.getClasses().stream().map(ClassResponseDTO::new).toList() : null,
-                course.getSections() != null ? course.getSections().stream().map(SectionResponseDTO::new).toList() : null);
+                course.getSections() != null ? course.getSections().stream().map(SectionResponseDTO::new)
+                        .sorted((section1, section2) ->  {
+                            return section1.id().compareTo(section2.id());
+                        }
+                        ).toList() : null);
     }
 
     public CourseResponseDTO(Course course, List<Section> sections) {
