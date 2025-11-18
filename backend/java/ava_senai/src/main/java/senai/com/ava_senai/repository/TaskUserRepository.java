5b82f90f4d1b9822613bfd33929fb794207f8bd5
nicolas <nicolas.souza@moinformatica.com>
2025-11-18 20:27:15 -0300

    feat: Creating ranking and implementing ranking logic

--- backend/java/ava_senai/src/main/java/senai/com/ava_senai/repository/TaskUserRepository.java
+++ backend/java/ava_senai/src/main/java/senai/com/ava_senai/repository/TaskUserRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import senai.com.ava_senai.domain.task.taskuser.TaskUser;
 
 public interface TaskUserRepository extends JpaRepository<TaskUser, Long> {
-    Boolean existsByTaskIdAndUserId(Long taskId, Long userId);
 
     Optional<TaskUser> findByUserIdAndTaskId(Long userId, Long taskId);
 
